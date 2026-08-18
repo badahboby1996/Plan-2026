d7d61aebe87c641f305c17d51993b289a91c406d
badahboby1996 <badahboby1996@gmail.com>
2026-08-18 18:11:15 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-/* Firebase Web конфигурацията от свързания проект Plan-2026. */
-window.HUSTLE_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDbNlAhKcToIYevZAXqMuEtgrcRem1Y0iE",
-  authDomain: "hustle-8134b.firebaseapp.com",
-  projectId: "hustle-8134b",
-  storageBucket: "hustle-8134b.firebasestorage.app",
-  messagingSenderId: "645064549363",
-  appId: "1:645064549363:web:c915071934186eb0efe886"
-};
